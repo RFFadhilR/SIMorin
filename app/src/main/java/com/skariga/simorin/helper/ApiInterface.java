89b41eb11c692e24cc5547fd7a7a27103c914f8a
RFFadhilR <rffr.dws.asa@mgmail.com>
2020-11-10 21:10:13 +0700

    Update List Jurnal Pembimbing Perusahaan

--- app/src/main/java/com/skariga/simorin/helper/ApiInterface.java
+++ app/src/main/java/com/skariga/simorin/helper/ApiInterface.java
@@ -6,16 +6,17 @@ import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface ApiInterface {
 
-    @GET("list-absen")
+    @POST("list-absen")
     Call<List<Absen>> getAbsens(
             @Query("id_pembimbing_perusahaan") String id_pembimbing_perusahaan
     );
 
-    @GET("list-jurnal")
+    @POST("list-jurnal")
     Call<List<Jurnal>> getJurnals(
             @Query("id_pembimbing_perusahaan") String id_pembimbing_perusahaan
     );
